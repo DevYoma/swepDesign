3c139a0ded6cf733d7476a82e528ff225abcb4d4
DevYoma <Lawrenceyoma@gmail.com>
2022-01-07 14:30:42 +0100

    done with Contact and About page, now on to AgentListing

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,30 +1,35 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Footer from './Footer';
+import Agent from './Agent';
 import '../Styles/About.css'
 
 const About = () => {
     return ( 
         <React.Fragment>
             <Navbar />
-            <div className="learnMore">
-                <h1>Do you have an</h1>
-                <h1>accomodation or rent ?</h1>
+            <div className="aboutMore">
+                <div>
+                    <div className='aboutMore__info'>
+                        <h1>Do you have an</h1>
+                        <h1>accomodation or rent ?</h1>
+                    </div>
 
-                <button>
-                    Become an agent
-                </button>
+                    <button>
+                        Become an agent
+                    </button>
 
-                <div className="learnMore__backgroundImg">
                 </div>
 
-                <div className="learnMore__more">
+                <div className="aboutMore__backgroundImg">
+                </div>
+
+                <div className="aboutMore__more">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus natus, itaque officia culpa nobis error sapiente commodi odio tempora quidem omnis earum dolorem illum, magni quaerat, architecto quas. Dolorem magni magnam culpa, asperiores harum ratione eos iure voluptatum consequuntur voluptatem numquam accusantium nobis saepe, consequatur obcaecati laboriosam? Eos, aperiam placeat!
                 </div>
-
-                {/* <div className="learnMore__button">
-                    <button>Go to Marketplace</button>
-                </div> */}
-        </div>
+            </div>
+            <Agent />
+            <Footer />
         </React.Fragment>
      );
 }

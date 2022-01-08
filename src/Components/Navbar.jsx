cd6a143894a43a69a86513689289f503549671bf
DevYoma <Lawrenceyoma@gmail.com>
2022-01-08 07:52:25 +0100

    WIP on AgentListingPage

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../Styles/Navbar.css'
 import Logo from '../assets/logo.png';
 import LogoName from '../assets/logoName.png';
+import AgentLogo from '../assets/agentLogo.png';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
@@ -10,6 +11,7 @@ const Navbar = () => {
             <div className="nav__left">
                 <img src={Logo} alt="logo" />
                 <img src={LogoName} alt="logo" />
+                {/* dynamically change the image in react */}
             </div>            
 
             <div className="nav__right">
@@ -17,7 +19,7 @@ const Navbar = () => {
                     <Link to={'/'}>Home</Link>
                 </li>
                 <li>
-                    <Link to={'/marketplace'}>Market Place</Link>
+                    <Link to={'/agentListing'}>Agent Listing</Link>
                 </li>
                 <li>
                     <Link to={'/about'}>About</Link>
